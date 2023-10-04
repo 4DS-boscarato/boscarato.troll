d50a23f051e6343a60f94dad2b8d91bf0593cd2d
andrea <andrea.boscarato@buonarroti.tn.it>
2023-10-04 15:23:26 +0200

    Feat: Creazione menu` per la scelta del menu`

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,12 @@ public class Main {
 
         Troll t1 = new Troll();
         boolean finisched = false;
+        boolean sceltaValida = false;
 
         String eroe = "";
         float pvEroe = 100;
 
-        System.out.println("Inserisci il tuo nome, Eroe: ");
+        System.out.print("Inserisci il tuo nome, Eroe: ");
         input = new Scanner(System.in);
         eroe = input.nextLine();
 
@@ -18,9 +19,37 @@ public class Main {
                 "\nOggi il famigerato Troll di nome " + t1.getNome() + " dovra` scontrarsi con il nostro nuovo campione di quest'anno, cioeeee` " + eroe +
                 "\nCHE LO SCONTRO ABBIAAAAAA INIZIOOO!!");
 
-        System.out.println("\n\nGAME");
+        System.out.println("\n\nGAME\n");
 
-        do{
+        do {
+            int scelta = 0;
+            do{
+                System.out.println("\nCosa vuoi fare eroe?");
+                System.out.println("1) Attacco leggero" +
+                        "\n2) Attacco pesante" +
+                        "\n3) Attacco super mega iper Pesante" +
+                        "\n4) Curata col falo` tattico");
+
+                System.out.print("\nInserisci la scela: ");
+                input = new Scanner(System.in);
+                scelta = input.nextInt();
+
+                if(scelta <= 4 && scelta >= 1) {
+                    sceltaValida = true;
+                }
+                else{
+                    System.out.println("A quanto pare abbaimo un eroe che non sa leggere i numeri!" +
+                            "\nRifai!");
+                }
+            }while(!sceltaValida);
+
+            switch(scelta) {
+                case 1: {
+
+                }
+                default:
+            }
+
 
         }while(!finisched);
     }
