30eeb42ac66c2cf06be10761e5c8177b3b4050f6
andrea <andrea.boscarato@buonarroti.tn.it>
2023-10-04 14:42:25 +0200

    Crazione del Metodo Costruttore e Finalizzazione attributi

---
+++ src/Troll.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Troll {
+    private String nome;
+    private float pv;
+    private float pd;
+
+
+    public Troll(){
+        pv = 100;
+        do{
+            pd = (int)((Math.random()*20));
+        }while(pd >= 5);
+        nome = "Rabid";
+    }
+}
