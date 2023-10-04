390ce910e3c0dbe3285e97b081432078f88a5331
andrea <andrea.boscarato@buonarroti.tn.it>
2023-10-04 16:29:29 +0200

    FINAL COMMIT 2

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,6 @@ public class Main {
         System.out.println("\nCommentatore: \nBuongiorno a tutti Signori e Signore! Benvenuti in questo nuovo combattimento nell'arena di Verona! " +
                 "\nOggi il famigerato Troll di nome " + t1.getNome() + " dovra` scontrarsi con il nostro nuovo campione di quest'anno, cioeeee` " + eroe +
                 "\nCHE LO SCONTRO ABBIAAAAAA INIZIOOO!!");
-
         System.out.println("\n\nGAME\n");
 
 
