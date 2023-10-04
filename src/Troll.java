486f53a6a8dd5f3cc29babd7e9187e1ab13cab3d
andrea <andrea.boscarato@buonarroti.tn.it>
2023-10-04 15:02:50 +0200

    Fix: Corretta la formula del random che decide i danni del Troll e sistemati i danni minimim

--- src/Troll.java
+++ src/Troll.java
@@ -6,20 +6,18 @@ public class Troll {
     private float pd;
 
 
-    public Troll(){
-        pv = 100;
-        do{
-            pd = (int)((Math.random()*30));
-        }while(pd >= 5);
+    public Troll() {
         nome = "Rabid";
+        pv = 100;
+
+        do{
+            pd = (float) ((Math.random() * 30));
+        }while (pd >= 10);
     }
 
     public void danniSubiti(float danniAttacco){
         pv -= danniAttacco;
     }
-
-
-
 
     public String getNome() {
         return nome;
