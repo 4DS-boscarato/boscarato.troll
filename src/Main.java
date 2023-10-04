1e0456fb50e1ddb929e80cd7c74724974683e508
andrea <andrea.boscarato@buonarroti.tn.it>
2023-10-04 15:06:09 +0200

    Feat: Creazio parte iniziale del gioco

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        Troll t1 = new Troll();
+        boolean finisched = false;
+
+        String eroe = "";
+        float pvEroe = 100;
+
+        System.out.println("Inserisci il tuo nome, Eroe: ");
+        input = new Scanner(System.in);
+        eroe = input.nextLine();
+
+        System.out.println("\nCommentatore: \nBuongiorno a tutti Signori e Signore! Benvenuti in questo nuovo combattimento nell'arena di Verona! " +
+                "\nOggi il famigerato Troll di nome " + t1.getNome() + " dovra` scontrarsi con il nostro nuovo campione di quest'anno, cioeeee` " + eroe +
+                "\nCHE LO SCONTRO ABBIAAAAAA INIZIOOO!!");
+
+        System.out.println("\n\nGAME");
+
+        do{
+
+        }while(!finisched);
     }
 }
